b2900a3b98c445f0ad1172a1856965c8ec0de855
Matthew Sadler <matthew.g.sadler@gmail.com>
2020-10-16 16:56:26 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz4320
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
